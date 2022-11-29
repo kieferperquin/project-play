c4b1cc80e70a33e705bb670aa2ecc2f1bcb2b79c
StonedBoi <75484259+jochemvdsteen@users.noreply.github.com>
2022-11-29 18:02:13 +0100

    Movement

---
+++ Assets/Jochem/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+
+    float speed;
+    float jumpforce;
+
+    int jumpCount = 1;
+
+    Rigidbody2D _rb2D;
+
+    void Start()
+    {
+        speed = 5f;
+        jumpforce = 8f;
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        if (jumpCount > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb2D.AddForce(jumpforce * Vector3.up, ForceMode2D.Impulse);
+            jumpCount -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 1;
+        }
+    }
+
+    /*private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+        }
+    }*/
+}
