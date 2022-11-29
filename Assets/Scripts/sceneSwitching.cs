5b17848de5f6305fb87545e270619364d4ff2241
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2022-11-29 16:32:08 +0100

    scenes + scene switching
    made all the scenes and made it so we can switch between scenes

---
+++ Assets/Scripts/sceneSwitching.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitching : MonoBehaviour
+{
+    public void fight()
+    {
+        SceneManager.LoadScene("characterSelect");
+    }
+    public void home()
+    {
+        SceneManager.LoadScene("start");
+    }
+    public void options()
+    {
+        SceneManager.LoadScene("options");
+    }
+    public void controls()
+    {
+        SceneManager.LoadScene("controls");
+    }
+    public void mapselect()
+    {
+        SceneManager.LoadScene("mapSelect");
+    }
+    public void map1()
+    {
+        SceneManager.LoadScene("map1");
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
