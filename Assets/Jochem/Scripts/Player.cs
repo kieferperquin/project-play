fa48c170df81847bf464b3c17fd0818d3513520f
StonedBoi <75484259+jochemvdsteen@users.noreply.github.com>
2022-11-30 11:58:23 +0100

    platforms

--- Assets/Jochem/Scripts/Player.cs
+++ Assets/Jochem/Scripts/Player.cs
@@ -9,15 +9,17 @@ public class Player : MonoBehaviour
     float speed;
     float jumpforce;
 
-    int jumpCount = 1;
+    int jumpCount = 2;
 
     Rigidbody2D _rb2D;
+    Collider2D playerCollider;
 
     void Start()
     {
         speed = 5f;
         jumpforce = 8f;
         _rb2D = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -28,7 +30,12 @@ public class Player : MonoBehaviour
         if (jumpCount > 0 && Input.GetKeyDown(KeyCode.Space))
         {
             _rb2D.AddForce(jumpforce * Vector3.up, ForceMode2D.Impulse);
-            jumpCount -= 1;
+            jumpCount =- 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _rb2D.AddForce(10 * Vector3.down, ForceMode2D.Impulse);
         }
 
         if (Input.GetKeyDown(KeyCode.A))
@@ -39,21 +46,23 @@ public class Player : MonoBehaviour
         {
             GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
         }
+
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform"))
+        {
+            jumpCount = 2;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform"))
         {
             jumpCount = 1;
         }
     }
-
-    /*private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            jumpCount = 0;
-        }
-    }*/
 }
