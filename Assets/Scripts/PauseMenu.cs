26b226353003a886abc7283042e70dffec594c7f
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2022-11-30 10:36:04 +0100

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour // script for pause menu
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+    public void resume()
+    {
+        anim.SetBool("pause", false);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void pause()
+    {
+        anim.SetBool("pause", true);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
