a268b7fadf5c25d9574295561a5ab0f4375d32dd
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2022-12-13 16:19:25 +0100

    made the sliders actualy change a value

---
+++ Assets/Scripts/soundSlider.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+
+public class soundSlider : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer Mixer;
+    [SerializeField]
+    private AudioSource AudioSource;
+    [SerializeField]
+    private TextMeshProUGUI ValueText;
+    [SerializeField]
+    private AudioMixMode MixMode;
+
+    public void OnChangeSlider(float Value)
+    {
+        ValueText.SetText($"{Value.ToString("N4")}");
+
+        switch(MixMode)
+        {
+            case AudioMixMode.LinearAudioSourceVolume:
+                AudioSource.volume = Value;
+                break;
+            case AudioMixMode.LinearMixerVolume:
+                Mixer.SetFloat("Volume", (-80 + Value * 100));
+                break;
+            case AudioMixMode.LogrithmicMixerVolume:
+                Mixer.SetFloat("Volume", Mathf.Log10(Value) * 20);
+                break;
+        }
+    }
+
+
+    public enum AudioMixMode
+    {
+        LinearAudioSourceVolume,
+        LinearMixerVolume,
+        LogrithmicMixerVolume
+    }
+}
