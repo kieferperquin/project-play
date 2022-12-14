d10fac0ee696f711c44a662d04d99a1bb4e8e118
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2022-12-14 12:48:40 +0100

    added music

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -5,6 +5,9 @@ public class AudioSettings : MonoBehaviour
 
     private static readonly string BackgroundPref = "BackgroundPref";
     private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
 
     void Awake()
     {
@@ -13,12 +16,14 @@ public class AudioSettings : MonoBehaviour
 
     private void ContinueSettings()
     {
+        backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+        soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
 
-        backgroundAudio.volume = backgroundSlider.value;
+        backgroundAudio.volume = backgroundFloat;
 
         for (int i = 0; i < soundEffectsAudio.Length; i++)
         {
-            soundEffectsAudio[i].volume = soundEffectsSlider.value;
+            soundEffectsAudio[i].volume = soundEffectsFloat;
         }
     }
 }
