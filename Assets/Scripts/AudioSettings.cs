eca94d043535aad6809f8abe744c2c45d66f71c9
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2022-12-14 11:48:53 +0100

    .

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPref";
+
+    void Awake()
     {
-        
+        ContinueSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ContinueSettings()
     {
-        
+
+        backgroundAudio.volume = backgroundSlider.value;
+
+        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = soundEffectsSlider.value;
+        }
     }
 }
