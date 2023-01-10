82bf54fa1cdafb32b414f83e5f3462ee68ed7596
Jochem van der Steen <jochemvdsteen@kpnmail.nl>
2023-01-10 13:56:13 +0100

    +

--- Assets/Jochem/Scripts/PlayerMovement1.cs
+++ Assets/Jochem/Scripts/PlayerMovement1.cs
@@ -17,7 +17,7 @@ public class PlayerMovement1 : MonoBehaviour
 
     [SerializeField] private TrailRenderer tr;
 
-    playerPref Player1 = new playerPref(3, 6f, 5f, 0f, 12f, 2, 25f, 0.1f, 1f);
+    playerPref Player1 = new playerPref(3, 6f, 5f, 0f, 12f, 2, 30f, 0.1f, 1f);
 
     private GameObject FrontAttack;
     private GameObject UpAttack;
